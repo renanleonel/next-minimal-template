7bc3495b0cfd0dd8521ab0c2965f5b10990146e5
renanleonel <renanleonelpro@gmail.com>
2025-04-27 11:49:21 -0300

    feat: upgrade deps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,3 @@
-'use client';
-
-import { Input } from '@/components/ui/input';
-
 export default function Home() {
-  return (
-    <div className='flex h-screen min-h-screen items-center justify-center bg-black'>
-      <div className='w-full max-w-md'>
-        <Input
-          type='text'
-          placeholder='Enter your name'
-          className='text-white'
-        />
-      </div>
-    </div>
-  );
+  return <div className='flex min-h-screen bg-[#101010]' />;
 }
